c990062baf10a0873502d96350803a3d816e4628
Guido Espana <confunguido@gmail.com>
2020-05-22 21:02:40 -0400

    Adding some preliminary results from likelihood including testing

--- scripts/report_figure_calibrated_parameters.R
+++ scripts/report_figure_calibrated_parameters.R
@@ -53,17 +53,18 @@ fred_sweep_df = fred_sweep_df %>% group_by(seed,state_name) %>% mutate(CumCF = c
 ##===============================
 col_palette = brewer.pal(n = 7, name='Dark2')
 jpeg('../figures/report_figure_calibrated_parameters.jpeg', width=7,height=5, units = "in", res = 300)
-layout(matrix(1:4,nrow = 2, byrow = T))
+layout(matrix(c(1,1,2,2,3,3,6,4,4,5,5,6),nrow = 2, byrow = T))
 par(mar = c(1,3,3,2), oma = c(1,4,0,1))
 particles_sampled_df = tibble()
 
 state_fred = fred_sweep_df %>%    
-    group_by(intervention_name, seed, state_name) %>%
+    group_by(intervention_name, state_name, job_id, seed) %>%
     summarize(H_sheltering_mean = max(H_sheltering_mean),
               shelter_in_place_compliance = mean(shelter_in_place_compliance),
               influenza_transmissibility = mean(influenza_transmissibility),
               imports_factor = mean(imports_factor),
-              influenza_asymp_infectivity = mean(influenza_asymp_infectivity)
+              influenza_asymp_infectivity = mean(influenza_asymp_infectivity),
+              tests_ratio = mean(tests_ratio)
               ) %>%
         ungroup()%>%
     filter(intervention_name %in% c("ShelterAll"))
@@ -141,6 +142,26 @@ axis(side = 1, at = 1:nrow(interventions_df), labels = interventions_df$State, c
 for(nn in 1:nrow(interventions_df)){
     print(sprintf("Asymptomatic infectivity %s",interventions_df$State[nn]))
     shelter_data = filter(state_fred, state_name == interventions_df$State[nn]) %>% pull(influenza_asymp_infectivity)
+    boxplot(x = shelter_data,
+            at = nn, add = T,
+            col = sprintf("%s80",col_palette[nn]),
+            outcol = col_palette[nn],
+            outpch = 16,
+            border = col_palette[nn],
+            medcol = col_palette[nn],
+            pars = list(xaxt = 'n', yaxt = 'n', axes = F), lwd = 1)
+    print(quantile(shelter_data, probs=c(0.025,0.5,0.975)))        
+}
+
+plot(-100, -100, xlim = c(0.5, nrow(interventions_df) + 0.5), 
+     ylim = c(min(state_fred$tests_ratio, na.rm = T),
+              max(state_fred$tests_ratio, na.rm = T)),
+     las = 2, xaxt = 'n', xlab = '', ylab = '', cex = 0.5)
+mtext(side = 2, "Adjustment parameter for testing", line = 3, cex = 0.7)
+axis(side = 1, at = 1:nrow(interventions_df), labels = interventions_df$State, cex.axis = 0.6)
+for(nn in 1:nrow(interventions_df)){
+    print(sprintf("Ratio %s",interventions_df$State[nn]))
+    shelter_data = filter(state_fred, state_name == interventions_df$State[nn]) %>% pull(tests_ratio)
     boxplot(x = shelter_data,
             at = nn, add = T,
             col = sprintf("%s80",col_palette[nn]),
--- scripts/report_figure_positive_tests.R
+++ scripts/report_figure_positive_tests.R
@@ -28,11 +28,21 @@ if(!is.null(attr(data_sys, "status"))){
 if(!file.exists('../../data/US_states_covid_data.csv')){
     stop("Something went wrong with downloading incidence data\n")
 }
+
+download.file('https://covidtracking.com/api/v1/states/daily.csv',
+              '../../data/US_states_testing_data.csv')
+
+us_testing = read_csv('../../data/US_states_testing_data.csv') %>%
+    dplyr::select(date, state, positiveIncrease, negativeIncrease) %>%
+    rename(positive=positiveIncrease,negative=negativeIncrease,State=state) %>%
+    mutate(date = lubridate::ymd(date))
+
 us_states_temp = read_csv('../../data/US_states_covid_data.csv')
 
 us_states = us_states_temp %>%
     filter(state %in% interventions_df$state_name) %>%
     left_join(interventions_df, by = c("state" = "state_name")) %>%
+    left_join(us_testing,by = c("date", "State")) %>%
     group_by(State) %>%
     mutate(cases_inc = c(cases[1], diff(cases)),
            deaths_inc = c(deaths[1], diff(deaths))) %>%
@@ -41,6 +51,7 @@ us_states = us_states_temp %>%
 fit_data = read_csv('../../experiments/Midwest_simulations/output/CALIBRATION/US_states_covid_data.csv') %>%
     filter(state %in% interventions_df$state_name) %>%
     left_join(interventions_df, by = c("state" =  "state_name")) %>%
+    left_join(us_testing,by = c("date", "State")) %>%
     group_by(State) %>%
     mutate(cases_inc = c(cases[1], diff(cases)),
            deaths_inc = c(deaths[1], diff(deaths))) %>%
@@ -72,7 +83,7 @@ fred_sweep_df = fred_sweep_df %>% group_by(seed,state_name) %>% mutate(CumCF = c
 ## PLOT Deaths by state --------
 ##===============================
 col_palette = brewer.pal(n = 7, name='Dark2')
-jpeg('../figures/report_figure_deaths_forecast.jpeg', width=7,height=5, units="in", res = 300)
+jpeg('../figures/report_figure_positive_tests.jpeg', width=7,height=5, units="in", res = 300)
 par(mar = c(3,2,3,1), oma = c(4,4,0,1))
 layout(matrix(c(1,1,2,2,3,3,4,4,8,5,5,6,6,7,7,8),nrow = 2, byrow = T))
 ##layout(matrix(c(1,1,2,2,3,3),nrow = 1, byrow = T))
@@ -80,43 +91,52 @@ particles_sampled_df = tibble()
 times_to_plot = seq(from=as.Date('2020-02-06'),to=as.Date('2020-06-15'), by = 1)
 x_inds = seq(from=times_to_plot[1],to=times_to_plot[length(times_to_plot)],length.out = 8)
 xlab_str = gsub(" +", " ", format(x_inds, "%b %e"))
+sens = 0.777; spec = 1.0
 
 for(nn in 1:nrow(interventions_df)){
     ss = interventions_df$State[nn]
     print(ss)
     st_intervention_tmp = filter(interventions_df, State == ss)
-    state_fred = filter(fred_sweep_df, state_name == ss)   
+    state_fred = filter(fred_sweep_df, state_name == ss)
+
+    state_fred$prevalence = state_fred$Is_mean / state_fred$N_mean
+    state_fred$infected_prev = (state_fred$I_mean - state_fred$Is_mean) / state_fred$N_mean
+    
+    state_fred$probC = state_fred$prevalence / (state_fred$prevalence + state_fred$tests_ratio * (1 - state_fred$prevalence))
+    
+    state_fred$probI = state_fred$infected_prev*state_fred$tests_ratio / (state_fred$prevalence + state_fred$tests_ratio * (1 - state_fred$prevalence))
+    state_fred$probU = (1 - state_fred$infected_prev - state_fred$prevalence)*state_fred$tests_ratio / (state_fred$prevalence + state_fred$tests_ratio * (1 - state_fred$prevalence))    
+    
+    state_fred$pos_test = (state_fred$probC + state_fred$probI) * sens + state_fred$probU*(1 - spec)
+
+    
     interv_sc = "ShelterAll"
     intervention_fred = filter(state_fred, intervention_name == interv_sc )
     intervention_end = as.Date(st_intervention_tmp$end_shelter_date[1])
     
     tmp_fred = intervention_fred %>%
         group_by(Day, Date, start_date, school_closure_day, shelter_in_place_delay_mean) %>%
-        summarize(CF_median = quantile(CF_mean, probs = c(0.5), na.rm = T),
-                  CF_low = quantile(CF_mean, probs = c(0.025), na.rm =T),
-                  CF_high = quantile(CF_mean, probs = c(0.975), na.rm = T),
-                  Cs_median = quantile(Cs_mean, probs = c(0.5)),
-                  Cs_low = quantile(Cs_mean, probs = c(0.025)), Cs_high = quantile(CF_mean, probs = c(0.975)),
-                  AR_low = quantile(AR_mean, probs = c(0.025)), AR_high = quantile(AR_mean, probs = c(0.975)),
-                  AR_median = quantile(AR_mean, probs = c(0.5))) %>%
+        summarize(pos_test_median = quantile(pos_test, probs = c(0.5), na.rm = T),
+                  pos_test_low = quantile(pos_test, probs = c(0.025), na.rm = T),
+                  pos_test_high = quantile(pos_test, probs = c(0.975), na.rm = T)) %>%
         ungroup()
 
-    plot(tmp_fred$Date, tmp_fred$CF_median, xaxs = "i", yaxs = "i", type = "l", lwd = 2, col = col_palette[nn],
+    plot(tmp_fred$Date, tmp_fred$pos_test_median, xaxs = "i", yaxs = "i", type = "l", lwd = 2, col = col_palette[nn],
          xlab = "", ylab = "", xlim = c(times_to_plot[1],times_to_plot[length(times_to_plot)]),
-         ylim = c(0,max(intervention_fred$CF_mean)), xaxt = 'n', yaxt = 'n')
+         ylim = c(0,1.0), xaxt = 'n', yaxt = 'n')
     mtext(side = 3, text = ss)
-    axis(side = 2, las = 2, at = seq(0, max(intervention_fred$CF_mean), length.out = 6),
-         labels = signif(round(seq(0, max(intervention_fred$CF_mean), length.out = 6)), 2))
+    axis(side = 2, las = 2, at = seq(0, 1.0, length.out = 6),
+         labels = signif(seq(0, 1.0, length.out = 6), 2))
 
     abline(v = as.Date(tmp_fred$start_date[1] + tmp_fred$school_closure_day[1] ), lty = 2, col = "navy", lwd = 1.5)
     abline(v = as.Date(tmp_fred$start_date[1] + tmp_fred$shelter_in_place_delay_mean[1]), lty = 3, col = "navy", lwd = 1.5)
     
     polygon(x = c(tmp_fred$Date, rev(tmp_fred$Date)),
-            y = c(tmp_fred$CF_high,
-                    rev(tmp_fred$CF_low)),
+            y = c(tmp_fred$pos_test_high,
+                    rev(tmp_fred$pos_test_low)),
             border = adjustcolor(col_palette[nn], alpha.f = 0.7),
             col = adjustcolor(col_palette[nn], alpha.f = 0.2))    
-    lines(tmp_fred$Date, tmp_fred$CF_median, lwd = 1.5, 
+    lines(tmp_fred$Date, tmp_fred$pos_test_median, lwd = 1.5, 
           col = adjustcolor(col_palette[nn], alpha.f = 0.2)) 
 
     tmp_data_fit = filter(fit_data, State == ss)
@@ -126,13 +146,14 @@ for(nn in 1:nrow(interventions_df)){
     abline(v = intervention_end, col = "navy", lwd = 1.5)
     
     tmp_data = filter(us_states, State == ss,date > tmp_data_fit$date[nrow(tmp_data_fit)], date < as.Date(Sys.Date()))
-    points(tmp_data$date, tmp_data$deaths_inc, col = "navy", lwd = 0.3, pch = 9, cex = 0.8)
-    
-    points(tmp_data_fit$date, tmp_data_fit$deaths_inc, col = "navy", lwd = 0.3,pch = 18)
+    ## points(tmp_data$date, tmp_data$deaths_inc, col = "navy", lwd = 0.3, pch = 9, cex = 0.8)    
+    points(tmp_data_fit$date, tmp_data_fit$positive / (tmp_data_fit$positive + tmp_data_fit$negative), col = "navy", lwd = 0.3,pch = 18)
 }
 
-mtext("Deaths", side = 2, line = 2, outer = T)
+mtext("Proportion of positive tests", side = 2, line = 2, outer = T)
 mtext("Date", side = 1, line = 2, outer = T)
 
 dev.off()
+
+
 
